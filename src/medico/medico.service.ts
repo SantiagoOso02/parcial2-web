5d9f247569b399f54813cf5ad4ed88ceebba4bdd
Santiago Andrés Osorio Osorio <s.osorioo@uniandes.edu.co>
2024-11-22 19:40:22 -0500

    ajustes medico service

--- src/medico/medico.service.ts
+++ src/medico/medico.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { MedicoEntity } from './medico.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  BusinessError,
+  BusinessLogicException,
+} from '../shared/errors/business-errors';
 
 @Injectable()
-export class MedicoService {}
+export class MedicoService {
+  constructor(
+    @InjectRepository(MedicoEntity)
+    private readonly medicoRepository: Repository<MedicoEntity>,
+  ) {}
+
+  async create(medico: MedicoEntity): Promise<MedicoEntity> {
+    if (medico.nombre.length === 0)
+      throw new BusinessLogicException(
+        'The medic name cannot be empty',
+        BusinessError.PRECONDITION_FAILED,
+      );
+    if (medico.especialidad.length === 0)
+      throw new BusinessLogicException(
+        'The medic specialty cannot be empty',
+        BusinessError.PRECONDITION_FAILED,
+      );
+    return await this.medicoRepository.save(medico);
+  }
+
+  async findOne(id: string): Promise<MedicoEntity> {
+    const medico: MedicoEntity = await this.medicoRepository.findOne({
+      where: { id },
+      relations: ['pacientes'],
+    });
+    if (!medico)
+      throw new BusinessLogicException(
+        'The medic with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    return medico;
+  }
+
+  async findAll(): Promise<MedicoEntity[]> {
+    return await this.medicoRepository.find({
+      relations: ['pacientes'],
+    });
+  }
+
+  async delete(id: string) {
+    const medico: MedicoEntity = await this.medicoRepository.findOne({
+      where: { id },
+      relations: ['pacientes'],
+    });
+    if (!medico)
+      throw new BusinessLogicException(
+        'The medic with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    if (medico.pacientes.length >= 1)
+      throw new BusinessLogicException(
+        'The medic has one or more patients, deletion is not allowed.',
+        BusinessError.PRECONDITION_FAILED,
+      );
+    await this.medicoRepository.remove(medico);
+  }
+}

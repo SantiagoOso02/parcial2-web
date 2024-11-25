1f20d8f4d71efccd931cd5a22f24662cf0d90611
Santiago Andrés Osorio Osorio <s.osorioo@uniandes.edu.co>
2024-11-24 19:39:18 -0500

    parte 2 terminada

---
+++ src/medico/medico.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class MedicoDto {
+  @IsString()
+  readonly nombre: string;
+
+  @IsString()
+  readonly especialidad: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly telefono: string;
+}
--- src/paciente/paciente.module.ts
+++ src/paciente/paciente.module.ts
@@ -3,9 +3,11 @@ import { PacienteService } from './paciente.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PacienteEntity } from './paciente.entity';
 import { DiagnosticoEntity } from '../diagnostico/diagnostico.entity';
+import { PacienteController } from './paciente.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PacienteEntity, DiagnosticoEntity])],
   providers: [PacienteService],
+  controllers: [PacienteController],
 })
 export class PacienteModule {}
